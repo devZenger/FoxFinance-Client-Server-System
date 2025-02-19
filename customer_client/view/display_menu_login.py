2724b67c480b74205959ad806ed89d0153ec15cc
devZenger <alwayscarryatowel@mail.de>
2025-02-19 15:49:14 +0100

    Add display_menu_login

---
+++ customer_client/view/display_menu_login.py
@@ -0,0 +1,17 @@
+from .display_menu_form import DisplayMenuForm
+
+class DisplayMenuLogin(DisplayMenuForm):
+    def __init__(self, menu_title, menu_points, form_names, to_fill, info=""):
+        self.form_names = form_names
+        self.to_fill = to_fill
+        super().__init__(menu_title, menu_points, info)
+    
+    
+    def execute(self):
+        self.display_title()
+        
+        self.display_info()
+        
+        self.display_form()
+        
+        self.display_option()
