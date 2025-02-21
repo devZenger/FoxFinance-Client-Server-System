294124dff992b942d709a405f4ab3f720a5326cf
devZenger <alwayscarryatowel@mail.de>
2025-02-21 15:57:27 +0100

    Add import_stock_data.py to import stock prices

---
+++ server/database/sqlite_scripts/import_stock_data.py
@@ -0,0 +1,39 @@
+import yfinance as yf
+import os, sys, sqlite3
+
+try: 
+    connection = sqlite3.connect("../FoxFinanceData.db")
+    cursor = connection.cursor()
+    print("verbunden")
+except:
+    print("Konnte keine Verbindung herstellen")
+
+cursor.execute("SELECT * FROM stocks")
+
+result = cursor.fetchall()
+
+for dsatz in result:
+    isin = dsatz[0]
+    ticker_symbol = f"{dsatz[1]}.DE"
+    name = dsatz[2]
+ 
+    try:
+        ticker_data = yf.Ticker(ticker_symbol)
+        historical_data = ticker_data.history(period='2y')
+    except:
+        print("Fehler konnte keine Daten zum Ticker Symbol finden")
+        
+    for i, row in historical_data.iterrows():
+        wrote = False
+        try:
+            cursor.execute("INSERT INTO stock_data(isin, date, open, high, low, close, volume, dividends, stock_splits) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (isin, i.strftime('%Y-%m-%d'), row['Open'], row['High'], row['Low'], row['Close'], row['Volume'], row['Dividends'], row['Stock Splits']))
+            connection.commit()
+            if wrote == False:
+                print(f"Daten eingegeben für {name}, isin {isin}")
+                wrote = True
+        except:
+            print(f"Fehler Daten von {name} isin: {isin} konnte nicht eingetragen werden")
+
+connection.commit()
+connection.close()
+
