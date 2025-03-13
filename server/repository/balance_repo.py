18f2d03c3beda02a8d9da0202c6d5549a3f4f318
devZenger <alwayscarryatowel@mail.de>
2025-03-13 12:54:51 +0100

    Add current balance und past balance transactions to server

--- server/repository/balance_repo.py
+++ server/repository/balance_repo.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 from .db_executor import DBExecutor
-from .search_repo import make_dictionary_one_result
+from .search_repo import make_dictionary_one_result, make_dictionary
 
 db_ex = DBExecutor()
 
@@ -38,11 +38,58 @@ def customer_balance(customer_id):
     except Exception as e:
         dic = {}
         print(f"Fehler customer balance: {e}")
-        raise e
+        raise ValueError(e)
     
     finally:
         db_ex.close()
         return dic
+
+
+def search_past_balance_transactions(customer_id, search_start, search_end):
+    
+    db_ex.open_connection_db()
+    
+    try: 
+        sql="""SELECT
+                    bt.balance_transaction_id,
+                    bt.balance_transaction_date,
+                    btt.type_of_action,
+                    bt.balance_sum,
+					bt.bank_account
+                    FROM (
+                        SELECT * 
+                        FROM balance_transactions AS bt
+                        WHERE bt.customer_id = ? 
+                            AND bt.balance_transaction_date >= ?
+                            AND bt.balance_transaction_date <= ?
+                        ORDER BY bt.balance_transaction_date ASC
+                    ) AS bt
+                    LEFT JOIN (
+                        SELECT btt.balance_transaction_type_id, btt.type_of_action
+                        FROM balance_transactions_type AS btt
+                        )AS btt
+                        ON bt.balance_transaction_type_id = btt.balance_transaction_type_id"""
+        
+
+        value = (customer_id, search_start, search_end,)
+        datas = db_ex.execute(sql, value).fetchall()
+            
+        names = db_ex.col_names()
+        
+        result = make_dictionary(datas, names)
+
+        print(f"result ist : {result}")
+        
+        return result
+
+    except Exception as e:
+        print(f"postion: search_past_balance_transactions, Error: {e}")
+        raise e
+
+    finally:
+        db_ex.close()
+
+
 
 
 
