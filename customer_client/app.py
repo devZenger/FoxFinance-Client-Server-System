1be7284098e052c5d02963efa15670ff7cb6c22f
devZenger <alwayscarryatowel@mail.de>
2025-06-11 21:39:44 +0200

    Clear terminal screen at program start

--- customer_client/app.py
+++ customer_client/app.py
@@ -1,10 +1,16 @@
+import os
 from controller.main_controller import MainControl
 
 
 def main():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
     start = MainControl()
     start.run()
 
 
 if __name__ == "__main__":
+
     main()
